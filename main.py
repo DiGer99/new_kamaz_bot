a2462d7b105f1f25042c51c14781dc912d69e28f
DiGer99 <gerasimov.dima891@gmail.com>
2025-03-01 09:38:44 +0300

    replace time 9:00

--- main.py
+++ main.py
@@ -24,8 +24,9 @@ async def main():
     dp = Dispatcher()
     
     scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
-    scheduler.add_job(scheduler_tommorow_button, "cron", hour=21, args=([bot]))
     scheduler.add_job(scheduler_today_button, "cron", hour=9, args=([bot]))
+    scheduler.add_job(scheduler_tommorow_button, "cron", hour=21, args=([bot]))
+    
     scheduler.start()
 
     dp.include_routers(handler_router)
